80e793e9869466fecb140aea77b2ceb6a7352332
petraalpigiano <petraalpigiano@gmail.com>
2025-04-23 15:41:01 +0200

    add call of API

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 // Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+axios
+  .get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+  .then((response) => {
+    console.log(response.data.response);
+  });
